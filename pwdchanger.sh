a03dd7d29ab83ff005ae238e373f4be820f9ef03
Konstantin Ivanov <kaivanov@gmail.com>
2013-07-09 13:45:43 -0500

    Update pwdchanger.sh

--- pwdchanger.sh
+++ pwdchanger.sh
@@ -6,7 +6,7 @@ char=(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A 
 max=${#char[*]}
 
 
-for i in `mysql -pc@m3yt1t $DB -e "SELECT url FROM servers ORDER BY url ASC" | grep -v url | awk '{print $1}' | tr -s \\n`
+for i in `mysql -pXXXX $DB -e "SELECT url FROM servers ORDER BY url ASC" | grep -v url | awk '{print $1}' | tr -s \\n`
 do
         password="";
         for j in `seq 1 $pwdlen`
@@ -18,6 +18,6 @@ do
         echo "Changing root password for $i to $password"
         ssh -p 1964 $i "echo $password | passwd --stdin root"
         echo "Updating winston's DB"
-        mysql -pc@m3yt1t $DB -e "UPDATE servers SET system_user=aes_encrypt('root / $password', '94*\$HN^3)}%d@jdf88-lcx') WHERE url='$i'"
+        mysql -pXXXX $DB -e "UPDATE servers SET system_user=aes_encrypt('root / $password', '94*\$HN^3)}%d@jdf88-lcx') WHERE url='$i'"
         echo "----------------------------"
 done
