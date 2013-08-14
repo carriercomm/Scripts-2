e331af53cf57106a1c02af5cebc503a02d84ee69
Konstantin Ivanov <kaivanov@gmail.com>
2013-08-14 14:26:08 -0500

    Added debugging and optimizations

--- GLB/geo_dns_worker.py
+++ GLB/geo_dns_worker.py
@@ -4,6 +4,7 @@ import MySQLdb
 from random import choice
 from pymemcache.client import *
 import math
+import datetime
 
 
 class DNSManipulator(object):
@@ -24,16 +25,16 @@ class DNSManipulator(object):
         #    C8     A5         01       00       00       01      00        00       00       00      00        00        03       77      77        77      06
         #    64     65         65       6B       6F       6E      03        63       6F       6D      00        00        02       00      01
 
-        if self.glb_config.debug:
-            data_hex = self.data.encode("hex")
-            data_bin = ""
-            for i in range(0, len(data_hex), 2):
-                data_bin += (bin(int(data_hex[i:i+2], 16)))[2:].zfill(8)
+        #if self.glb_config.debug:
+        #    data_hex = self.data.encode("hex")
+        #    data_bin = ""
+        #    for i in range(0, len(data_hex), 2):
+        #        data_bin += (bin(int(data_hex[i:i+2], 16)))[2:].zfill(8)
 
-            log_event = "Hex Data: " + data_hex
-            self.logger.info(log_event)
-            log_event = "Binary Data: " + data_bin
-            self.logger.info(log_event)
+        #    log_event = "Hex Data: " + data_hex
+        #    self.logger.info(log_event)
+        #    log_event = "Binary Data: " + data_bin
+        #    self.logger.info(log_event)
 
         dns_type1 = ord(self.data[-4:-3])      # Get the most  significant byte from the Question Type
         dns_type2 = ord(self.data[-3:-2])      # Get the least significant byte from the Question Type
@@ -59,9 +60,6 @@ class DNSManipulator(object):
 
         dns_type_hex = self.data[-4:-2].encode("hex")
 
-        if self.glb_config.debug:
-            self.logger.info("Record Type: %s %s %s" % (dns_type, dns_type_hex, record_type))
-
         return record_type
 
     def get_domain_name(self):
@@ -77,9 +75,6 @@ class DNSManipulator(object):
                 ini += lon + 1
                 lon = ord(self.data[ini])
                 self.domain_name_offset = ini
-            if self.glb_config.debug:
-                log_event = "Domain name offset = " + str(self.domain_name_offset)
-                self.logger.info(log_event)
 
         return domain_name
 
@@ -87,8 +82,9 @@ class DNSManipulator(object):
 
         # TODO: Pick a DNS server from a list of servers defined in the config file based on health check status
 
-        log_event = "Quering DNS server " + str(self.glb_config.dns_backends[0]) + " for " + domain_name + " type " + dns_type
-        self.logger.info(log_event)
+        if self.glb_config.debug:
+            log_event = "Quering DNS server " + str(self.glb_config.dns_backends[0]) + " for " + domain_name + " type " + dns_type
+            self.logger.debug(log_event)
 
         ip_list = []
 
@@ -101,10 +97,11 @@ class DNSManipulator(object):
                 if res_list[i]["typename"] == "A":
                     ip_list.append(res_list[i]["data"])
 
-            log_event = "DNS server returned " + str(ip_list)
-            self.logger.info(log_event)
+            if self.glb_config.debug:
+                log_event = "DNS server returned " + str(ip_list)
+                self.logger.debug(log_event)
         except (DNS.Base.SocketError) as dns_error:
-                self.logger.info("Cant connect to back-end DNS server - %s" % (dns_error))
+                self.logger.error("Cant connect to back-end DNS server - %s" % (dns_error))
 
         return ip_list
 
@@ -122,6 +119,9 @@ class DNSManipulator(object):
             requestor_location  = memcache_client.get(requestor_key)                       # Check memcached for the requestors location, if not query the database and update the cache
 
             if requestor_location == None:
+                if self.glb_config.debug:
+                    log_event = "Requestor location not found in cache"
+                    self.logger.debug(log_event)
                 statement = "select country_code from " + self.glb_config.geoip_country + " where INET_ATON('" + requestor + "') BETWEEN begin_ip_num AND end_ip_num"
                 cur.execute(statement)
                 rows = cur.fetchall()
@@ -130,16 +130,28 @@ class DNSManipulator(object):
                     memcache_client.set(requestor_key, requestor_location, 3 * 60)     # Cache the requestors location for 3 * 60 seconds
                 else:
                     requestor_location = "Unknown"
+            else:
+                if self.glb_config.debug:
+                    log_event = "Requestor location " + str(requestor_location) + " found in cache"
+                    self.logger.debug(log_event)
 
             for i in range(0, len(dns_resolved_list)):
                 a_record_key = "a_record_" + str(dns_resolved_list[i])
                 selected_geo_loc = memcache_client.get(a_record_key)
                 if selected_geo_loc == None:
+                    if self.glb_config.debug:
+                        log_event = str(a_record_key) + " not found in cache"
+                        self.logger.debug(log_event)
                     continue
                 elif selected_geo_loc == requestor_location:
+                    if self.glb_config.debug:
+                        log_event = str(a_record_key) + " found in cache and matches the requestor location"
+                        self.logger.debug(log_event)
                     selected_geo_ip = dns_resolved_list[i]
                     break
 
+            if self.glb_config.debug:
+                        start_op = datetime.datetime.now()
             if selected_geo_ip == "Nowhere" or selected_geo_ip == None:
                 for i in range(0, len(dns_resolved_list)):
                     statement = "select country_code from " + self.glb_config.geoip_country + " where INET_ATON('" + dns_resolved_list[i] + "') BETWEEN begin_ip_num AND end_ip_num"
@@ -154,6 +166,12 @@ class DNSManipulator(object):
                             selected_geo_ip = dns_resolved_list[0]
                     else:
                         selected_geo_ip = dns_resolved_list[0]
+            if self.glb_config.debug:
+                end_op = datetime.datetime.now()
+                time_op = end_op - start_op
+                log_event = "Total time to lookup all A records in the database: " + str(time_op.seconds) + " seconds and " + str(time_op.microseconds) + " microseconds"
+                self.logger.debug(log_event)
+
                 memcache_client.set("a_record_" + selected_geo_ip, selected_geo_loc, 3 * 60)     # Cache the A record location for 3 * 60 seconds
 
             cur.close()
@@ -161,18 +179,20 @@ class DNSManipulator(object):
             memcache_client.close()
 
         except (MemcacheUnexpectedCloseError, MySQLdb.OperationalError, socket.error) as conn_error:
-            logger.info(conn_error)
+            self.logger.error(conn_error)
 
-        log_event = "Requestor " + requestor + " located at " + requestor_location
-        self.logger.info(log_event)
-        log_event = "Sending to " + selected_geo_ip + " at " + selected_geo_loc
-        self.logger.info(log_event)
+        if self.glb_config.debug:
+            log_event = "Requestor " + requestor + " located at " + requestor_location
+            self.logger.debug(log_event)
+            log_event = "Sending to " + selected_geo_ip + " at " + selected_geo_loc
+            self.logger.debug(log_event)
 
         return selected_geo_ip
 
     def get_coordinates(self, ipaddress):
         coordinates = None
-
+        if self.glb_config.debug:
+            start_op = datetime.datetime.now()
         con = MySQLdb.connect(self.glb_config.geoip_store_host, self.glb_config.geoip_store_user, self.glb_config.geoip_store_pass, self.glb_config.geoip_store_db)
         cur = con.cursor(MySQLdb.cursors.DictCursor)
 
@@ -188,14 +208,22 @@ class DNSManipulator(object):
                 requestor_lon = rows[0]['longitude']
                 requestor_lat = rows[0]['latitude']
                 coordinates = rows[0]
-                log_event = "Coordinates for " + str(ipaddress) + " " + str(requestor_lon) + ":" + str(requestor_lat)
-                self.logger.info(log_event)
+                if self.glb_config.debug:
+                    log_event = "Coordinates for " + str(ipaddress) + " " + str(requestor_lat) + ":" + str(requestor_lon)
+                    self.logger.debug(log_event)
         else:
-            log_event = "Unknown coordinates for " + str(ipaddress)
-            self.logger.info(log_event)
+            if self.glb_config.debug:
+                log_event = "Unknown coordinates for " + str(ipaddress)
+                self.logger.debug(log_event)
 
         cur.close()
         con.close()
+
+        if self.glb_config.debug:
+            end_op = datetime.datetime.now()
+            time_op = end_op - start_op
+            log_event = "Total time to get coordinates for " + str(ipaddress) + ": " + str(time_op.seconds) + " seconds and " + str(time_op.microseconds) + " microseconds"
+            self.logger.debug(log_event)
 
         return coordinates
 
@@ -216,6 +244,10 @@ class DNSManipulator(object):
         requestor_lat = 0
         a_records_distance = {}
 
+        if self.glb_config.debug:
+            log_event = "Requestor: " + requestor
+            self.logger.debug(log_event)
+
         requestor_coordinates = self.get_coordinates(requestor)
         if requestor_coordinates != None:
             requestor_lon = requestor_coordinates['longitude']
@@ -230,13 +262,22 @@ class DNSManipulator(object):
                     a_record_lat = a_record_coordinates['latitude']
 
                     # Calculate distance from the requestor to the A record and add to a dictionary that contains A records and the distances from the requstor
+                    if self.glb_config.debug:
+                        start_op = datetime.datetime.now()
                     a_records_distance[dns_resolved_list[i]] = self.get_distance(requestor_lon, requestor_lat, a_record_lon, a_record_lat)
+                    if self.glb_config.debug:
+                        end_op = datetime.datetime.now()
+                        time_op = end_op - start_op
+                        log_event = "Total time to calculate distance for " + str(dns_resolved_list[i]) + ": " + str(time_op.seconds) + " seconds and " + str(time_op.microseconds) + " microseconds"
+                        self.logger.debug(log_event)
             if len(a_records_distance) > 0:
-                log_event = "List of distances : " + str(a_records_distance)
-                self.logger.info(log_event)
+                if self.glb_config.debug:
+                    log_event = "List of distances : " + str(a_records_distance)
+                    self.logger.debug(log_event)
                 selected_geo_ip = [key for (key,value) in a_records_distance.items() if value == sorted(a_records_distance.values())[0]][0]
-                log_event = "Closest A record is: " + str(selected_geo_ip)
-                self.logger.info(log_event)
+                if self.glb_config.debug:
+                    log_event = "Closest A record is: " + str(selected_geo_ip)
+                    self.logger.debug(log_event)
         else:
             selected_geo_ip = dns_resolved_list[0]
 
@@ -245,10 +286,11 @@ class DNSManipulator(object):
     def random_select(self, requestor, dns_resolved_list):                 # For the given list of A records, select one at random
         selected_random_ip = choice(dns_resolved_list)
 
-        log_event = "Requestor " + requestor
-        self.logger.info(log_event)
-        log_event = "Sending to random server " + selected_random_ip
-        self.logger.info(log_event)
+        if self.glb_config.debug:
+            log_event = "Requestor " + requestor
+            self.logger.debug(log_event)
+            log_event = "Sending to random server " + selected_random_ip
+            self.logger.debug(log_event)
 
         return selected_random_ip
 
